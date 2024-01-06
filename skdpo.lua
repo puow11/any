2da41386a1ecc74786a8f0e8b602809c040e7a34
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-06 19:03:32 +0700

    Update skdpo.lua

--- skdpo.lua
+++ skdpo.lua
@@ -1,14 +1,3 @@
-repeat wait()
-until game:IsLoaded()
-local TableChat = {"script by m.1.M ~ https://dsc. gg/m1m-link","script by m.1.M ~ https://dsc. gg/m1m-link"}
-spawn(function()
-    while wait() do 
-        pcall(function()
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(TableChat[math.random(1,#TableChat)],"All")
-            wait(45)
-        end)
-    end
-end)
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 repeat task.wait() until game:IsLoaded()
 local TablePlace = {13772394625,2753915549,7041939546,8737899170,155382109,8737602449,13333567382,15002061926}

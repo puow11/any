babe0ce293bc1d5c981730366ac2b4df72914fde
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-06 17:58:28 +0700

    Update skdpo.lua

--- skdpo.lua
+++ skdpo.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/m1M-Plqer819/hubmlk/main/prte/rp0d.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/m1M-Plqer819/skiddu/main/repl7/rp0d.lua"))();

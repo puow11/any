018d08752154a1dd8b95401576297da67fd6e6ca
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-07 16:49:29 +0700

    Update skdpo.lua

--- skdpo.lua
+++ skdpo.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/m1M-Plqer819/skiddu/main/repl7/rp0d.lua"))()
+repeat task.wait() until game:IsLoaded()
+local TablePlace = {13772394625,2753915549,7041939546,8737899170,155382109,8737602449,13333567382,15002061926}
+if table.find(TablePlace,game.PlaceId) then 
+elseif game.PlaceId == 13710713070 or game.PlaceId == 13213667041 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/m1M-Plqer819/skiddu/main/repl7/rp0d.lua"))()
+else
+    game.Players.LocalPlayer:Kick("not Support")
+end

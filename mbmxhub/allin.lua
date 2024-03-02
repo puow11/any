bdd0d357a9824dd3100f9b74635b978412c88371
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-03-02 17:19:02 +0700

    Update allin.lua

--- mbmxhub/allin.lua
+++ mbmxhub/allin.lua
@@ -1,1 +1,7 @@
-
+--[[
+    script made by Dakii & Minh -- or call me by the name you like (@dakii_90790 & @player.81 in discord🤡)
+    18/10/ ? VN
+          9 8 0 1 2 3 4 5 6 7 
+    bio : https://bio.site/m1m - my server : https://dsc.gg/m1m-link
+    mbm x hub - blox fruits
+--]]

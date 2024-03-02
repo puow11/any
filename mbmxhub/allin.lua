83e6c21de0eb2732ae18774fe37942844a586a83
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-03-02 17:18:24 +0700

    Update allin.lua

--- mbmxhub/allin.lua
+++ mbmxhub/allin.lua
@@ -1,17 +1,1 @@
-        repeat task.wait() until game:IsLoaded()
-        local TablePlace = {7449423635,2753915549,4442272183}
-        if table.find(TablePlace,game.PlaceId) then 
-            if _G.Mode == "Vn" then 
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/DakiiHub/dakiihub/main/main.lua", true))()
-            elseif _G.Mode == "Chest" then
-                _G.Mode = "Pirates"
-                loadstring(game:HttpGet('https://raw.githubusercontent.com/m1M-Plqer819/any/main/mbmxhub/atcbm.lua'))()
-            else
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/m1M-Plqer819/any/main/mbmxhub/0135"))()
-            end
-        elseif game.PlaceId == 13772394625 or game.PlaceId == 14915220621 or game.PlaceId == 14732610803 or game.PlaceId ==  15264892126 then
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/m1M-Plqer819/any/main/mbmxhub/blballs%3B'))()
-        else
-            setclipboard("https://discord.gg/kuXFw2HkdN")
-            game.Players.LocalPlayer:Kick("Check script here : https://discord.gg/kuXFw2HkdN")
-        end
+
